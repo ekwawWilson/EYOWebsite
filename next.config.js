0ba2ff7b6f9fda698c744829d4b8b27051f1ab4f
wilson <ee.wilson@outlook.com>
2025-05-11 14:58:01 +0000

    10 comit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export", // ✅ Enables static HTML export
+  trailingSlash: true, // ✅ Optional: ensures URLs end with /
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
